8c70ef240f8ca896e7953d1d77897960b998be11
EdgarVMora <cansiller.ed@gmail.com>
2024-12-01 16:13:06 -0600

    se agrego notificacion de campo requerido y mejora visual en css ejercicio cuatro

--- src/ejercicioCuatro/mejorasFormulario.jsx
+++ src/ejercicioCuatro/mejorasFormulario.jsx
@@ -7,6 +7,7 @@ const MejorasFormulario = () => {
     apellido: "",
     correo: "",
     contrasena: "",
+    genero: "",
   })
 
   const [fieldTouched, setFieldTouched] = useState({
@@ -14,8 +15,29 @@ const MejorasFormulario = () => {
     apellido: false,
     correo: false,
     contrasena: false,
+    genero: false,
   })
 
+  const [errorMessage, setErrorMessage] = useState ({
+    nombre: "",
+    apellido: "",
+    correo: "",
+    contrasena: "",
+    genero: "",
+  })
+
+  const validateField = (name, value) => {
+    let message = ""
+    if(!value){
+        message = "Este campo es obligatorio"
+    } else if (name === "contrasena" && value.length < 6) {
+        message = "Debe tener al menos 6 caracteres";
+      }
+    setErrorMessage((prev)=> ({
+        ...prev,
+        [name]: message,
+    }))
+  }
 
   const isFormComplete =
     formData.nombre && formData.apellido && formData.correo && formData.contrasena
@@ -27,15 +49,17 @@ const MejorasFormulario = () => {
       ...prev,
       [name]: value,
     }))
+    validateField(name, value)
   }
 
 
   const handleBlur = (e) => {
-    const { name } = e.target;
+    const { name, value } = e.target;
     setFieldTouched((prev) => ({
       ...prev,
       [name]: true,
     }))
+    validateField(name, value)
   }
 
 
@@ -47,17 +71,20 @@ const MejorasFormulario = () => {
   return (
     <div className="contenedor_login">
 <form className="estructura_login" onSubmit={handleSubmit}>
+    <h2>Registro</h2>
       <div className="campo">
         <label htmlFor="nombre">Nombre:</label>
         <input
           type="text"
           id="nombre"
           name="nombre"
+          placeholder="Ingresa tu nombre"
           value={formData.nombre}
           onChange={handleChange}
           onBlur={handleBlur}
           className={fieldTouched.nombre ? (formData.nombre ? "verde" : "rojo") : ""}
         />
+        {errorMessage && <span className="error">{errorMessage.nombre}</span>}
       </div>
       <div className="campo">
         <label htmlFor="apellido">Apellido:</label>
@@ -65,11 +92,13 @@ const MejorasFormulario = () => {
           type="text"
           id="apellido"
           name="apellido"
+          placeholder="Ingresa tus apellidos"
           value={formData.apellido}
           onChange={handleChange}
           onBlur={handleBlur}
           className={fieldTouched.apellido ? (formData.apellido ? "verde" : "rojo") : ""}
         />
+        {errorMessage && <span className="error">{errorMessage.apellido}</span>}
       </div>
       <div className="campo">
         <label htmlFor="correo">Correo:</label>
@@ -77,11 +106,13 @@ const MejorasFormulario = () => {
           type="email"
           id="correo"
           name="correo"
+          placeholder="Ingresa tu email"
           value={formData.correo}
           onChange={handleChange}
           onBlur={handleBlur}
           className={fieldTouched.correo ? (formData.correo ? "verde" : "rojo") : ""}
         />
+        {errorMessage && <span className="error">{errorMessage.email}</span>}
       </div>
       <div className="campo">
         <label htmlFor="contrasena">Contraseña:</label>
@@ -89,11 +120,100 @@ const MejorasFormulario = () => {
           type="password"
           id="contrasena"
           name="contrasena"
+          placeholder="Ingresa una contrasena"
           value={formData.contrasena}
           onChange={handleChange}
           onBlur={handleBlur}
           className={fieldTouched.contrasena ? (formData.contrasena ? "verde" : "rojo") : ""}
         />
+        {errorMessage && <span className="error">{errorMessage.contrasena}</span>}
+
+      </div>
+
+      <div className="campo">
+      <fieldset>
+                    <legend>Seleciona tu genero:</legend>
+                    <label htmlFor="male">
+                        <input 
+                            type="radio"
+                            name="genero"
+                            value="male"
+                            onChange={handleChange}
+                        /> 
+                        Masculino
+                    </label>
+                    <label htmlFor="female">
+                        <input 
+                            type="radio"
+                            name="genero"
+                            value="female"
+                            onChange={handleChange}
+                        />
+                        Femenino
+                    </label> 
+                    <label htmlFor="otro">
+                        <input 
+                            type="radio"
+                            name="genero"
+                            value="otro"
+                            onChange={handleChange}
+                        />
+                        Otro
+                    </label> 
+                </fieldset>
+                {errorMessage.genero && <span className="error">{errorMessage.genero}</span>}
+      </div>
+
+      <div className="campo">
+      <fieldset>
+                    <legend>Que opcion te interesa mas:</legend>
+                        <label htmlFor="sport">
+                            <input 
+                                type="checkbox"
+                                name="hobby"
+                                value="sport"
+                                id="sport"
+                                
+                            />
+                            Deportes
+                        </label>
+                        <label htmlFor="music">
+                            <input 
+                                type="checkbox"
+                                name="hobby"
+                                value="music"
+                                id="music"
+                                
+                            />
+                            Musica
+                        </label>
+                        <label htmlFor="art">
+                            <input 
+                                type="checkbox"
+                                name="hobby"
+                                value="art"
+                                id="art"
+                                
+                            />
+                            Arte
+                        </label>
+                         <label htmlFor="technology">
+                            <input 
+                                type="checkbox"
+                                name="hobby"
+                                value="technology"
+                                id="technology"
+                            />
+                            Tecnologia
+                        </label>
+                </fieldset>    
+      </div>
+      <div className="campo">
+      <label htmlFor="birthday">Seleciona tu fecha de nacimiento</label>
+                <input 
+                    type="date"
+                    id="birthday" 
+                />
       </div>
       <button type="submit" disabled={!isFormComplete}>
         Enviar
